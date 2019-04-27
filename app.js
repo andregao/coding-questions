04befc7b39fb7b0d76a86b7de92e57e0f4a6e5fd
Andre Gao <andregao@me.com>
2019-04-27 02:20:19 -0700

    recursion factorial

--- app.js
+++ app.js
@@ -1,52 +1,34 @@
-class Graph { //undirected, unweighted, using adjacent list
-  constructor() {
-    this.totalNodes = 0;
-    this.adjacentList = {}
-  }
-
-  addVertex(value) {
-    if (!this.adjacentList[value]) {
-      this.adjacentList[value] = [];
-      return this.totalNodes++;
+function findFactorialRecursive(number) {
+  // edge case
+  if (number < 1) {
+    if (number === 0) {
+      return 1;
+    }
+    if (number < 0) {
+      return null
     }
   }
-
-  addEdge(v1, v2) {
-    if (this.adjacentList[v1] && this.adjacentList[v2]) {
-      this.adjacentList[v1].push(v2);
-      this.adjacentList[v2].push(v1);
-    }
+  // base case
+  if (number === 1) {
+    return 1;
   }
-
-  showConnections() {
-    const allNodes = Object.keys(this.adjacentList);
-    for (let node of allNodes) {
-      let nodeConnections = this.adjacentList[node];
-      let connections = '';
-      let vertex;
-      for (vertex of nodeConnections) {
-        connections += vertex + ' ';
-      }
-      console.log(node + '-->' + connections);
-    }
-  }
+  // recursive case
+  return number * findFactorialRecursive(number - 1);
 }
 
-const graph = new Graph();
-graph.addVertex('0');
-graph.addVertex('1');
-graph.addVertex('2');
-graph.addVertex('3');
-graph.addVertex('4');
-graph.addVertex('5');
-graph.addVertex('6');
-graph.addEdge('3', '1');
-graph.addEdge('3', '4');
-graph.addEdge('4', '2');
-graph.addEdge('4', '5');
-graph.addEdge('1', '2');
-graph.addEdge('1', '0');
-graph.addEdge('0', '2');
-graph.addEdge('6', '5');
+function findFactorialIterative(number) {
+  let result = 1; // for both 0 and 1
+  if (number < 0) {
+    return null;
+  }
+  while (number > 1) {
+    result = result * number;
+    number--;
+  }
+  return result;
+}
 
-graph.showConnections();
+let target = 1;
+
+console.log(findFactorialRecursive(target));
+console.log(findFactorialIterative(target));
