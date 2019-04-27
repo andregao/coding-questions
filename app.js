deab42eafd796bbfc3ec3ab677824eb15e80bef5
Andre Gao <andregao@me.com>
2019-04-27 01:29:17 -0700

    graph with adjacent list

--- app.js
+++ app.js
@@ -1,159 +1,52 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinarySearchTree {
+class Graph { //undirected, unweighted, using adjacent list
   constructor() {
-    this.root = null;
+    this.totalNodes = 0;
+    this.adjacentList = {}
   }
 
-  insert(value) {
-    let node = new Node(value);
-    if (!this.root) { // if tree is empty
-      return this.root = node;
-    }
-    let current = this.root;
-    while (true) { //traverse
-      if (value < current.value) {
-        if (current.left) {
-          current = current.left
-        } else {
-          current.left = node;
-          return;
-        }
-      } else {
-        if (current.right) {
-          current = current.right
-        } else {
-          current.right = node;
-          return;
-        }
-      }
+  addVertex(value) {
+    if (!this.adjacentList[value]) {
+      this.adjacentList[value] = [];
+      return this.totalNodes++;
     }
   }
 
-  lookup(value) {
-    if (!this.root) {
-      return null;
+  addEdge(v1, v2) {
+    if (this.adjacentList[v1] && this.adjacentList[v2]) {
+      this.adjacentList[v1].push(v2);
+      this.adjacentList[v2].push(v1);
     }
-    let current = this.root;
-    // while (true) {
-    //   if (value === current.value) { // found
-    //     return current;
-    //   }
-    //
-    //   if (value < current.value) {
-    //     if (current.left) {
-    //       current = current.left;
-    //     } else {
-    //       return null
-    //     }
-    //   } else {
-    //     if (current.right) {
-    //       current = current.right;
-    //     } else {
-    //       return null
-    //     }
-    //   }
-    // }
-    while (current) { // traverse until the end
-      if (value === current.value) {
-        return current;
-      } else {
-        value < current.value
-          ? current = current.left
-          : current = current.right
-      }
-    }
-    return null
   }
 
-  remove(value) {
-    // search for node
-    if (!this.root) {
-      return null;
-    }
-    let current = this.root;
-    let parent, isLessThanParent;
-    while (current) { // traverse until the end
-      if (value === current.value) {
-        break; // found current
-      } else {
-        parent = current; // move on to a child
-        if (value < current.value) {
-          current = current.left;
-          isLessThanParent = true;
-        } else {
-          current = current.right;
-          isLessThanParent = false;
-        }
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = '';
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + ' ';
       }
+      console.log(node + '-->' + connections);
     }
-
-    // if value not found
-    if (!current) {
-      return null;
-    }
-
-    // if no child, delete
-    if (!current.left && !current.right) {
-      return assignNodeToParent(null, parent, isLessThanParent)
-    }
-    // if one child, bypass
-    if (!current.left || !current.right) {
-      return current.left
-        ? assignNodeToParent(current.left, parent, isLessThanParent)
-        : assignNodeToParent(current.right, parent, isLessThanParent);
-    }
-    // if two children, replace with successor
-    // find successor
-    let successor = current.right;
-    if (!successor.left) {
-      return successor.left = current.left;
-    }
-    let successorParent;
-    while (successor.left) { //traverse when successor has left child
-      successorParent = successor;
-      successor = successor.left;
-    }
-    // replace with successor
-    successor.right
-      ? successorParent.left = successor.right
-      : successorParent.left = null;
-
-    successor.left = current.left;
-    successor.right = current.right;
-    assignNodeToParent(successor, parent, isLessThanParent);
   }
 }
 
-function assignNodeToParent(node, parent, isLessThanParent) {
-  isLessThanParent
-    ? parent.left = node
-    : parent.right = node;
-}
+const graph = new Graph();
+graph.addVertex('0');
+graph.addVertex('1');
+graph.addVertex('2');
+graph.addVertex('3');
+graph.addVertex('4');
+graph.addVertex('5');
+graph.addVertex('6');
+graph.addEdge('3', '1');
+graph.addEdge('3', '4');
+graph.addEdge('4', '2');
+graph.addEdge('4', '5');
+graph.addEdge('1', '2');
+graph.addEdge('1', '0');
+graph.addEdge('0', '2');
+graph.addEdge('6', '5');
 
-const traverse = node => {
-  const tree = {value: node.value};
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
-};
-
-const tree = new BinarySearchTree();
-tree.insert(9);
-tree.insert(4);
-tree.insert(7);
-tree.insert(20);
-tree.insert(170);
-tree.insert(15);
-tree.insert(1);
-tree.insert(5);
-tree.insert(8);
-tree.remove(4);
-console.log(tree.root);
-
+graph.showConnections();
